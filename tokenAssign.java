de5c4f57b9f2a3e6058dc91d20340096d7f4d9bc
jev-e <jacob@jev.org.uk>
2020-03-04 21:11:57 +0000

    Add files via upload
    Updated Token class with placeholder image paths. 
    
    Added Class for assignment of Token to Player and creation of Player.

---
+++ tokenAssign.java
@@ -0,0 +1,72 @@
+/**
+ *
+ * @author Jacob Evans
+ *	Class / Method for creation of players
+ *	Dependant on token/player classes
+ */
+
+
+import java.util.Scanner;
+import java.lang.*;
+
+public class tokenAssign {
+    public static void main(String[] args) {
+        Player player1;
+        player1 = playerCreate();
+        System.out.println(player1.getName());
+        System.out.println(player1.getToken());
+        System.out.println(player1.getToken().getPath());
+    }
+
+    public static Player playerCreate() {
+        String newline = System.lineSeparator();
+        System.out.print("Please enter your name: ");
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.print("Your name is " + name + newline);
+        System.out.print(("Please pick your token from the options below:" + newline +
+                "1. Cat" + newline +
+                "2. Hatstand" + newline +
+                "3. Smartphone" + newline +
+                "4. Boot" + newline +
+                "5. Goblet" + newline +
+                "6. Spoon" + newline));
+        String playerChoice = in.nextLine();
+        Player newPlayer = null;
+        String tokenChoice = playerChoice.toUpperCase();
+        if ("CAT".equals(tokenChoice)) {
+            System.out.println("You have chosen the Cat token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+
+        } else if ("HATSTAND".equals(tokenChoice)) {
+            System.out.println("You have chosen the Hatstand token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+
+        } else if ("SMARTPHONE".equals(tokenChoice)) {
+            System.out.println("You have chosen the Smartphone token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+
+
+        } else if ("BOOT".equals(tokenChoice)) {
+            System.out.println("You have chosen the Boot token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+
+
+        } else if ("GOBLET".equals(tokenChoice)) {
+            System.out.println("You have chosen the Goblet token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+
+
+        } else if ("SPOON".equals(tokenChoice)) {
+            System.out.println("You have chosen the Spoon token.");
+            Token playerToken = Token.valueOf(tokenChoice);
+            newPlayer = new Player(name,playerToken);
+        }
+        return newPlayer;
+    }
+}
